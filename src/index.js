18c64f89e2825beff31be58bf5eeb7671c32310d
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-06-05 10:03:25 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 console.log('%c HI', 'color: firebrick')
+
+const url = "https://dog.ceo/api/breeds/image/random/4";
+const dogsUrl = 'https://dog.ceo/api/breeds/list';
+
+window.addEventListener('DOMContentLoaded', () => {
+  fetch(url).then(result => {return result.json()})
+  .then(json =>{
+    for(let i=0;i<json.message.length;i++){
+    let img = document.createElement("img");
+    img.setAttribute("src",json.message[i]);
+    let imgDiv = document.querySelector("#dog-image-container"); 
+    imgDiv.append(img);
+    imgDiv.style.display = "flex"
+    img.style.with = "300px"
+    imgDiv.style.with = "100px"
+    }
+  });
+
+  fetch(dogsUrl).then(result => {return result.json()})
+   .then(json =>{
+      for(let i=0;i<json.message.length;i++){
+
+        let breed = document.createElement("li");
+        breed.innerHTML = json.message[i];
+        breed.addEventListener("click",()=>{
+          breed.style.color = "blue";
+        });
+        
+        let list = document.querySelector("#dog-breeds");
+        list.append(breed);
+
+        let selector=document.querySelector("#breed-dropdown");
+        selector.addEventListener("change",function(){
+          for(let i=0; i<json.message.length; i++){
+           if(breed.innerHTML.startsWith(selector.value) ){
+             breed.style.display = "block";
+           }else{
+             breed.style.display = "none";
+           }
+          }
+        });
+      }
+   }); 
+});
